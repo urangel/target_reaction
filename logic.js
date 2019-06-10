f15eb3a9cf3f2d773f17792b4e055e04fa0958a9
Ulises <uli.atx@gmail.com>
2019-06-10 16:14:53 -0500

    game works, needs d3 plotting

--- logic.js
+++ logic.js
@@ -1,21 +1,19 @@
 
-let targetTotal = 10;
-let round = 0;
-let startTime, endTime, roundTime;
+let startTime, endTime, roundTime, fastest, slowest, average;
 let roundTimes = [];
 
 let targetArea = document.getElementById("target-area");
 let startButton = document.getElementById("start-button");
+let title = document.querySelector("div#title h1");
+
 
 function genTemp() {
   let defaultRow = ". . . . . . . . . . . .";
 
   let gridArr = [];
 
-  let targetPos = [Math.floor(Math.random()*12), Math.floor(Math.random()*12)];
-  let targetX = targetPos[0];
-  let targetY = targetPos[1];
-  console.log(targetX +1, targetY +1);
+  let targetX = Math.floor(Math.random()*12);
+  let targetY = Math.floor(Math.random()*12);
 
   for(let i =0; i<12; i++){
     if(i === targetY){
@@ -39,18 +37,31 @@ startButton.addEventListener("click", function(e){
   startButton.setAttribute("style", "display: none");
   //start timer
   startTime = Date.now();
-  console.log(Date.now());
 })
 
 document.addEventListener("DOMContentLoaded", function(){
 
   document.getElementById("target").addEventListener("click", function(e) {
     e.preventDefault();
-    endTime = Date.now();
-    roundTime = endTime - startTime;
-    roundTimes.push(roundTime);
-    startTime = Date.now();
-    genTemp();
+    
+    if(roundTimes.length < 10){
+      endTime = Date.now();
+      roundTime = endTime - startTime;
+      roundTimes.push(roundTime);
+      startTime = Date.now();
+      genTemp();
+  
+    } else {
+      title.innerHTML = "Game Over";
+      let averager = (array) => array.reduce((a, b) => a + b) / array.length;
+      average = parseInt(averager(roundTimes));
+      slowest = Math.max(...roundTimes);
+      fastest = Math.min(...roundTimes);
+      document.getElementById("fast-val").innerText = "Your fastest reaction was " + fastest + " ms";
+      document.getElementById("slow-val").innerText = "Your slowest reaction was " + slowest + " ms";
+      document.getElementById("average-val").innerText = "Your average reaction was " + average + " ms";
+
+    }
 
   })
 

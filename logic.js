7efe2fc86ed1bae0d48288c7d213730ac9301acd
Ulises <uli.atx@gmail.com>
2019-06-17 21:19:05 -0500

    bar chart to svg, styling changes

--- logic.js
+++ logic.js
@@ -2,13 +2,19 @@
 let startTime, endTime, roundTime, fastest, slowest, average;
 let roundTimes = [];
 
-let targetArea = document.getElementById("target-area");
-let startButton = document.getElementById("start-button");
-let title = document.querySelector("div#title h1");
-let stats = document.getElementById("stats");
+const targetArea = document.getElementById("target-area");
+const target = document.getElementById("target");
+const blank = document.getElementById("blank");
+const startButton = document.getElementById("start-button");
+const title = document.querySelector("div#title h1");
+const stats = document.getElementById("stats");
+const body = document.getElementsByTagName("body");
+const chart = document.getElementById("chart");
 
 
 function genTemp() {
+  startTime = Date.now();
+
   let defaultRow = ". . . . . . . . . . . .";
 
   let gridArr = [];
@@ -34,10 +40,10 @@ function genTemp() {
 
 startButton.addEventListener("click", function(e){
   e.preventDefault();
+  target.style.display = "block";
+  blank.style.display = "block";
   genTemp();
   startButton.setAttribute("style", "display: none");
-  //start timer
-  startTime = Date.now();
 })
 
 document.addEventListener("DOMContentLoaded", function(){
@@ -49,7 +55,6 @@ document.addEventListener("DOMContentLoaded", function(){
       endTime = Date.now();
       roundTime = endTime - startTime;
       roundTimes.push(roundTime);
-      startTime = Date.now();
       genTemp();
   
     } else {
@@ -58,25 +63,54 @@ document.addEventListener("DOMContentLoaded", function(){
       average = parseInt(averager(roundTimes));
       slowest = Math.max(...roundTimes);
       fastest = Math.min(...roundTimes);
-      document.getElementById("fast-val").innerText = "Your fastest reaction was " + fastest + " ms";
-      document.getElementById("slow-val").innerText = "Your slowest reaction was " + slowest + " ms";
-      document.getElementById("average-val").innerText = "Your average reaction was " + average + " ms";
+      document.getElementById("fast-val").innerText = "Fastest: " + fastest + " ms";
+      document.getElementById("slow-val").innerText = "Slowest: " + slowest + " ms";
+      document.getElementById("average-val").innerText = "Average: " + average + " ms";
+      stats.style["background-color"] = "#fff";
       targetArea.setAttribute("style", "display: none");
       stats.style.display = "flex";
+      stats.style["flex-direction"] = "column";
 
-      d3.select("#chart")
-      .selectAll("div")
-      .data(roundTimes)
-      .enter().append("div")
-      .style("width", function(d) { return d / 5 + "px"; })  //Here I need to normalize the width of the max element to 100% the size of the container and adjust all other divs according to that reference.
-      .text(function(d) { return d + " ms"; });
+
+      let svgWidth = chart.clientWidth, svgHeight = chart.clientHeight, barPadding = 5;
+      let barWidth = (svgWidth / roundTimes.length);
+
+      let svg = d3.select('svg')
+          .attr("width", svgWidth)
+          .attr("height", svgHeight);
+
+      let y = d3.scaleLinear()
+          .domain([0, d3.max(roundTimes)])
+          .range([0, svgHeight]);
+    
+      // let x = d3.scaleLinear()
+      //     .domain([1, 10])
+      //     .range([0, svgWidth]);
+
+      // let xAxis = d3.axisBottom()
+      //     .scale(x);
+
+      let barChart = svg.selectAll("rect")
+          .data(roundTimes)
+          .enter()
+          .append("rect")
+          .attr("y", function(d) {
+            return svgHeight - y(d);
+          })
+          .attr("height", function(d) { 
+            return y(d); 
+          }) 
+          .attr("width", barWidth - barPadding)
+          .attr("transform", function (d, i) {
+            let translate = [barWidth * i, 0]; 
+            return "translate("+ translate +")";
+          });
+
+          // svg.append("g")
+          //   .call(xAxis);
     }
 
   });
 
-  // document.getElementById("fastest").addEventListener("click", function(e){
-    
-  // });
-  
 });
 
